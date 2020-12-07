f6f88462a54f4fd045213eeb763479141f5dfdd1
yoni206 <yoni206@gmail.com>
2020-12-07 15:55:09 -0800

    traces

--- src/proof/lfsc/lfsc_printer.cpp
+++ src/proof/lfsc/lfsc_printer.cpp
@@ -224,7 +224,11 @@ void LfscPrinter::printProofInternal(
         {
           // a normal rule application, compute the proof arguments
           std::vector<PExpr> args;
-
+          Trace("proofs-debug") << std::endl << " ; proves: " << d_tproc.toInternal(cur->getResult()) << std::endl;
+          Trace("proofs-debug") << " ; assumes: " << std::endl;
+          for (auto dpn : cur->getChildren()) {
+            Trace("proofs-debug") << " ;   " << d_tproc.toInternal(dpn->getResult()) << std::endl;
+          }
           if (computeProofArgs(cur, args))
           {
             processedChildren[cur] = false;
