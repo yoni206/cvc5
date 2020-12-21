ebd71c46cdb3fc23d676583117dc13d1757e6657
ajreynol <andrew.j.reynolds@gmail.com>
2020-12-21 12:02:08 -0600

    Fix

--- src/prop/theory_proxy.h
+++ src/prop/theory_proxy.h
@@ -33,10 +33,6 @@
 #include "prop/sat_solver.h"
 #include "prop/theory_preprocess_solver.h"
 #include "theory/theory.h"
-<<<<<<< HEAD
-=======
-#include "theory/theory_preprocessor.h"
->>>>>>> ccda071a9605baa42602d580affa296cbc674807
 #include "theory/trust_node.h"
 #include "util/resource_manager.h"
 #include "util/statistics_registry.h"
